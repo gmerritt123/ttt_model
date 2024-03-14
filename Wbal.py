df3b10ad4ff50bdcb443762d658f5191167ce957
gmerritt123 <58000181+gmerritt123@users.noreply.github.com>
2024-03-14 16:25:23 -0400

    revamp
    built on the idea of wo_df and calc wbal, formalized two dataclasses (Rider and Paceline) and sketched out formulas.

--- Wbal.py
+++ Wbal.py
@@ -7,124 +7,188 @@ Created on Sat Feb 24 09:11:48 2024
 
 import pandas as pd
 import numpy as np
-
+from dataclasses import dataclass
 
 
 import matplotlib.pyplot as plt
 
-def build_wodf(wo,dur):
-    '''translates a "workout" {'Time':[x,y],'Power':[px,py]} into a dataframe given a total estimated duration.
-    workout will repeat itself until duration is used up
-    returns a dataframe
+def asymp_func(x,x0,y0,x1,y1):
     '''
-    df = pd.DataFrame(data=wo)
-    nrnds = int(np.ceil(dur/df['Time'].sum()))
-    df = pd.concat([df for x in range(nrnds)]).reset_index(drop=True)
-    df['cs'] = df['Time'].cumsum()
-    df = df[df.index <= df[df['cs']<dur].index.max() + 1]
-    df.iloc[-1]['Time'] = dur-df.iloc[-2]['cs']
-    return df.drop(columns='cs')
-
-def calcWbal(df,cp,wp):
-    '''given a df output from build_wodf, critical power (cp) and W' (wp), calculates Wbal over time @ 1 sec resolution)
-    stolen from here --> https://forum.intervals.icu/t/using-w-balance/804/4
+    used to help model draft position base array
+    x = array of positions to eval, then supply two values (0,1) and (7,0.55)
     '''
-    tdf = pd.DataFrame(data={'Time':np.arange(0,df['Time'].max()+1,1)})
-    df= pd.merge(df,tdf,how='right',on='Time')
-    df['Power'] = df['Power'].bfill()
-    wb = np.zeros(len(df))
-    for i in range(len(wb)):
-        if i == 0:
-            wb[i] = wp
-            continue
-        t = df['Time'].iloc[i]
-        deltaW = cp - df['Power'].iloc[i]
-        if deltaW > 0:
-            w = wb[i-1] + deltaW * (wp - wb[i-1])/wp
-        else:
-            w = wb[i-1]+deltaW
-        wb[i] = w
-    df['Wbal'] = wb
-    return df
+    f=1
+    a = -1
+    b = x1-x0
+    c = (x1*x0)-(f*x0-f*x1)/(y0-y1)
+    d = np.roots([a,b,c])[1]
+    e = y0 - (1/(x0-d))
+    res = 1/(x-d)+e
+    return res
 
-#application
+@dataclass
+class Rider:
+    name: str
+    wt: float
+    ht: float
+    ftp: float
+    wp: float 
+    
+    def calc_wbal(self):
+        '''
+        stolen from here --> https://forum.intervals.icu/t/using-w-balance/804/4
+        '''
+        df = self.wo_df.copy()
+        df['Time'] = df['Time'].cumsum()
+        tdf = pd.DataFrame(data={'Time':np.arange(0,df['Time'].max()+1,1)})
+        df= pd.merge(df,tdf,how='right',on='Time')
+        df['Power'] = df['Power'].bfill()
+        wb = np.zeros(len(df))
+        for i in range(len(wb)):
+            if i == 0:
+                wb[i] = self.wp
+                continue
+            t = df['Time'].iloc[i]
+            deltaW = self.ftp - df['Power'].iloc[i]
+            if deltaW > 0:
+                w = wb[i-1] + deltaW * (self.wp - wb[i-1])/self.wp
+            else:
+                w = wb[i-1]+deltaW
+            wb[i] = w
+        df['Wbal'] = wb
+        self.wbal = df
 
-turn_times = [90,60,75,75,30,20] #proposed turn times on the flat AL, GM, TR etc.
+@dataclass
+class Paceline:
+    riders: list
+    scaling_factor: float
+    
+    def __post_init__(self):
+        assert all([isinstance(x,Rider) for x in self.riders])
+        self.mean_ftp = np.mean([x.ftp for x in self.riders])
+        self.target_power = self.mean_ftp*self.scaling_factor
+        self.mean_wt = np.mean([x.wt for x in self.riders])
+        self.mean_ht = np.mean([x.ht for x in self.riders])
+        
+    def calc_power_demands(self,base_array,height_factor,weight_factor,draft_factor):
+        hf_array = (np.array([x.ht for x in self.riders])/self.mean_ht)**height_factor
+        wf_array = (np.array([x.wt for x in self.riders])/self.mean_wt)**weight_factor
+        f = hf_array * wf_array #represents "base scaling" based on height/weight --> i.e. best estimate of CdA
+        for i,r in enumerate(self.riders):
+            df = (f[i]*base_array)**draft_factor #additional scaling on each draft position based on CdA for that rider
+            p = f[i]*self.target_power*df
+            r.power_array = p
+        df = pd.DataFrame(index=[x.name for x in pl.riders]
+                          ,data=np.array([x.power_array for x in pl.riders])
+                          ,columns=[i for i in range(len(pl.riders))]).round(0).reset_index().rename(columns={'index':'rider'})
+        self.pwr_df = df       
 
-p1c = 5.5 #target w/kg of p1 rider on volcano climb
+    def build_wodf(self, dur, turn_times):
+        '''translates a "workout" {'Time':[x,y],'Power':[px,py]} into a dataframe given a total estimated duration.
+        workout will repeat itself until duration is used up
+        returns a dataframe
+        '''
+        
+        for i,r in self.pwr_df.iterrows():
+            #rearrange the power demands based on position
+            c = r[list(range(0,len(self.pwr_df)))].to_list()
+            pa = c[:i+1][::-1]+c[i+1:][::-1]          
+            df = pd.DataFrame(data={'Time':turn_times,'Power':pa})
+            nrnds = int(np.ceil(dur/df['Time'].sum()))
+            df = pd.concat([df for x in range(nrnds)]).reset_index(drop=True)
+            df['cs'] = df['Time'].cumsum()
+            df = df[df.index <= df[df['cs']<dur].index.max() + 1].reset_index(drop=True)
+            df['Time'].loc[-1] = dur-df['cs'].iloc[-2]
+            df = df.drop(columns='cs')
+            r = [x for x in self.riders if x.name == r['rider']][0]
+            if hasattr(r,'wo_df'):
+                r.wo_df = pd.concat([r.wo_df,df])
+            else:
+                r.wo_df = df
 
-cd =  {1:1.0,2:0.89,3:0.82,4:0.80,5:0.80,6:0.80} #estimate of w/kg reduction on climb by position
+r1 = Rider(name='AL',wt=60,ht=170,ftp=300,wp=25000)
+r2 = Rider(name='GM',wt=64,ht=173,ftp=297,wp=25000)
+r3 = Rider(name='DS',wt=81,ht=188,ftp=335,wp=25000)
+r4 = Rider(name='ER',wt=85,ht=184,ftp=322,wp=25000)
+r5 = Rider(name='JW',wt=78,ht=185,ftp=305,wp=25000)
+r6 = Rider(name='JM',wt=74,ht=171,ftp=260,wp=25000)
 
-#each rider's stats and proposed workout (two segments flat + volcano w estimated durations)
-#volcano segment plan is for GM and AL to swap pulls, everyone else hangs on
-
-rd = {'AL':{'segments':{'flat':{'Time':turn_times,'Power':[364,211,211,222,247,291],'dur':25*60}
-                                   ,'volcano':{'Time':[120,120],'Power':[290,340],'dur':7*60}} 
-                        ,'cp': 300
-                        ,'wp':30000
-                        ,'wt':60
-                        }
-      ,'GM':{'segments':{'flat':{'Time':turn_times,'Power':[298,372,216,216,227,253],'dur':25*60}
-                       ,'volcano':{'Time':[120,120],'Power':[350,300],'dur':7*60}} 
-            ,'cp': 300
-            ,'wp':30000
-            ,'wt':64
-            }
-      ,'TR':{'segments':{'flat':{'Time':turn_times,'Power':[271,319,399,243,231,231],'dur':25*60}
-                       ,'volcano':{'Time':[120,120],'Power':[360,360],'dur':7*60}} 
-            ,'cp': 323
-            ,'wp':30000
-            ,'wt':75
-            }
-      ,'SB':{'segments':{'flat':{'Time':turn_times,'Power':[237,264,311,388,225,225],'dur':25*60}
-                       ,'volcano':{'Time':[120,120],'Power':[334,334],'dur':7*60}} 
-            ,'cp': 299
-            ,'wp':30000
-            ,'wt':69.5
-            }
-      ,'SG':{'segments':{'flat':{'Time':turn_times,'Power':[222,233,260,306,382,222],'dur':25*60}
-                       ,'volcano':{'Time':[120,120],'Power':[320,320],'dur':7*60}} 
-            ,'cp': 275
-            ,'wp':30000
-            ,'wt':67.5
-            }
-     
-      ,'JW':{'segments':{'flat':{'Time':turn_times,'Power':[238,238,250,279,328,410],'dur':25*60}
-                             ,'volcano':{'Time':[120,120],'Power':[350,350],'dur':7*60}} 
-                  ,'cp': 297
-                  ,'wp':30000
-                  ,'wt':78
-                  }
-
-      }
-#janky way of updating the power demands on the volcano climb via adjusting ONLY p1c
-for i,k in enumerate(rd.keys()):
-    m = rd[k]['wt']
-    if i == 0:
-        rd[k]['segments']['volcano']['Power'] = [p1c*cd[i+1]*m,p1c*cd[i+2]*m]
-    elif i == 1:
-        rd[k]['segments']['volcano']['Power'] = [p1c*cd[i+1]*m,p1c*cd[i]*m]
-    else:
-        rd[k]['segments']['volcano']['Power'] = [p1c*cd[i+1]*m,p1c*cd[i+1]*m]
-
+pl = Paceline(riders=[r1,r2,r3,r4,r5,
+                        r6
+                      ]
+              ,scaling_factor=1.28)
+print('Mean Power at Front:')
+print(pl.target_power)
+barray = asymp_func(x=np.arange(0,len(pl.riders),1),x0=0,y0=1.0,x1=4,y1=0.62)
+#
+print('Draft Fractions by Position:')
+print(barray)
+pl.calc_power_demands(base_array=barray
+                     ,height_factor=0.2 #higher the number, the more penalty being tall is
+                     ,weight_factor=0.15 #higher then number, the more penalty being heavy is
+                     ,draft_factor=0.9 #the higher the number, the more draft advantage is given to smaller riders 
+                     )
+pl.build_wodf(dur=45*60,
+              turn_times=[90,60,60,45,45,5]
+              )
+print('Power Demands')
+print(pl.pwr_df)
 
 fig, ax = plt.subplots()
-
-#for each rider, stack the demands of each segment, and calc Wbal over time for them
-for r in rd.keys():
-    rdf = pd.DataFrame()
-    for seg,wo in rd[r]['segments'].items():
-        print(seg)
-        df = build_wodf(wo={k:wo[k] for k in ['Time','Power']},dur=wo['dur'])
-        rdf=pd.concat([rdf,df])
-    rdf['Time'] = rdf['Time'].cumsum()
-    df = calcWbal(df=rdf,cp=rd[r]['cp'],wp=rd[r]['wp'])
-    ax.plot(df['Time']/60,df['Wbal'],label=r)
-
+for r in pl.riders:
+    r.calc_wbal()
+    ax.plot(r.wbal['Time']/60,r.wbal['Wbal'],label=r.name)
 ax.legend()
 ax.set_ylabel('Wbal')
 ax.set_xlabel('Duration (min)')
 ax.set_ylim(0,30000)
 plt.grid()
+#%%
+pp_df = pl.pwr_df[['rider',0]].rename(columns={0:'Power (w)'})
+pp_df['Turn (s)'] = [90,60,60,45,45,5]
+
+
+ #%%           
+#"calibration" of height,weight and draft factors + base array
+# needs to be cleaned up/more formalized but rough idea for now
+# populate observations from riders e.g "in p3 i was averaging x today"
+
+def calcRes(a):         
+    r1 = Rider(name='AL',wt=60,ht=170,ftp=300,wp=30000)
+    r2 = Rider(name='GM',wt=64,ht=173,ftp=300,wp=30000)
+    r3 = Rider(name='DS',wt=81,ht=188,ftp=335,wp=30000)
+    r4 = Rider(name='DK',wt=75,ht=178,ftp=312,wp=30000)
+    r5 = Rider(name='CP',wt=80,ht=180,ftp=300,wp=30000)
+    r6 = Rider(name='SM',wt=82,ht=182,ftp=300,wp=30000)
+    r7 = Rider(name='ER',wt=85,ht=184,ftp=322,wp=30000)
+    r8 = Rider(name='MB',wt=77.6,ht=180,ftp=320,wp=30000)
+    
+    pl = Paceline(riders=[r1,r2,r3,r4,r5,r6,r7,r8],turn_times=[90,45,30],scaling_factor=1.3)
+    
+    height_factor = a[0]
+    weight_factor = a[1]
+    draft_factor = a[2]
+    pl.calc_power_demands(base_array=asymp_func(x=np.arange(0,7,1),x0=0,y0=1.0,x1=7,y1=0.57)
+                          ,height_factor=height_factor,weight_factor=weight_factor,draft_factor=draft_factor)
+    #observations
+    o1 = (r2.power_array[1] - 315)**2
+    o2 = (r2.power_array[5] - 245)**2
+    o3 = (r2.power_array[2] - 285)**2
+    o4 = (r3.power_array[1] - 380)**2
+    o5 = (r6.power_array[5] - 300)**2
+    o6 = (r7.power_array[5] - 300)**2
+    # o7 = (r7.power_array[1] - 360)**2
+    o8 = (r1.power_array[0] - 360)**2
+    return np.sum([o1,o2,o3,o4,o5,o6,o8])
+
+# r = calcRes(height_factor=1.05,weight_factor=0.1,draft_factor=0.75)
+
+from scipy.optimize import minimize
+
+ie = [0.2,0.3,0.75]
+res = minimize(calcRes,x0=ie)
+print(res.x)
+
+
     
     
