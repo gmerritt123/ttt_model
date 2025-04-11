8b230a019171e0f9ad484885a02d7ed62a3d07a8
gmerritt123 <h2merrit@gmail.com>
2025-04-11 15:09:58 -0400

    Needs cleanup

--- Wbal.py
+++ Wbal.py
@@ -9,12 +9,8 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 
-
 import matplotlib.pyplot as plt
 
-
-
-#%%
 def power_speed(v,cd,a,wc,wb,g,vhw=0,rho=1.22601,crr=0.004,lossdt=2,draft_factor=1):
     '''
     power given speed
@@ -37,6 +33,8 @@ def power_speed(v,cd,a,wc,wb,g,vhw=0,rho=1.22601,crr=0.004,lossdt=2,draft_factor
     return dtl_term * (g_term + a_term)*v
 
 
+
+
 def frontal_area(height,weight):
     '''https://www.researchgate.net/publication/7906171_The_Science_of_Cycling_Factors_Affecting_Performance_Part_2
     height in m
@@ -44,6 +42,13 @@ def frontal_area(height,weight):
     '''
     return 0.0293*height**0.725*weight**0.425+ 0.0604
 
+# power_speed(v=31.8/3.6,cd=0.7185
+#             ,a=frontal_area(1.73,64)
+#             ,wc=64
+#             ,wb=9.221
+#             ,g=1.7
+#             ,vhw=0,rho=1.22601
+#             ,crr=0.016,lossdt=2,draft_factor=1)
 
 def asymp_func(x,x0,y0,x1,y1):
     '''
@@ -101,7 +106,9 @@ class Paceline:
     riders: list
     target_speed: float
     grade: float
-    crr: float   
+    crr: float
+    distance: float
+    turn_times: list
     
     def __post_init__(self):
         assert all([isinstance(x,Rider) for x in self.riders])            
@@ -137,19 +144,22 @@ class Paceline:
                                  , wc=r.wt
                                  , wb=r.wb
                                  , g=self.grade
+                                 ,crr=self.crr
                                  ,draft_factor=b
                                  )
-                parr.append(p)
+                parr.append(np.max([0,p]))
             r.power_array = parr    
         df = pd.DataFrame(index=[x.name for x in pl.riders]
                           ,data=np.array([x.power_array for x in pl.riders])
                           ,columns=[i for i in range(len(pl.riders))]).round(0).reset_index().rename(columns={'index':'rider'})
         self.pwr_df = df       
 
-    def build_wodf(self, dur, turn_times):
+    def build_wodf(self):
         '''with power demands calculated, given a total duration and an array of turn times (len = len(self.riders)
         calculates a "workout_df" for each rider in the paceline        
         '''
+        dur = self.distance/self.target_speed*60*60 #hours to seconds
+        turn_times = self.turn_times
         for i,r in self.pwr_df.iterrows():
             #rearrange the power demands based on positioning
             c = r[list(range(0,len(self.pwr_df)))].to_list()
@@ -157,70 +167,92 @@ class Paceline:
             df = pd.DataFrame(data={'Time':turn_times,'Power':pa})
             nrnds = int(np.ceil(dur/df['Time'].sum()))
             df = pd.concat([df for x in range(nrnds)]).reset_index(drop=True)
+
             df['cs'] = df['Time'].cumsum()
             df = df[df.index <= df[df['cs']<dur].index.max() + 1].reset_index(drop=True)
-            df['Time'].loc[-1] = dur-df['cs'].iloc[-2]
+            df.loc[df['cs']>dur,'Time'] = (dur-df['cs'].shift(1)).round(0) #need to round to nearest second for wbal calc later..
             df = df.drop(columns='cs')
+
             r = [x for x in self.riders if x.name == r['rider']][0]
             if hasattr(r,'wo_df'):
                 r.wo_df = pd.concat([r.wo_df,df])
             else:
                 r.wo_df = df
 
+aero_rb = {'cd':0.814,'wb':6.636}
+tt_b = {'cd':0.7185,'wb':9.221}
 
+bike = tt_b
 #step one --> define riders, cd and wb are from zwifter bikes for cadex disc
-r1 = Rider(name='AL',wt=60,ht=170,ftp=295,wp=20000,cd=0.7185,wb=9.221)
-r2 = Rider(name='GM',wt=64,ht=173,ftp=305,wp=20000,cd=0.7185,wb=9.221)
-r3 = Rider(name='DS',wt=83,ht=188,ftp=350,wp=20000,cd=0.7185,wb=9.221)
 
-r4 = Rider(name='SG',wt=67,ht=179,ftp=290,wp=20000,cd=0.7185,wb=9.221)
-r5 = Rider(name='EK',wt=76,ht=183,ftp=315,wp=20000,cd=0.7185,wb=9.221)
-r6 = Rider(name='TR',wt=75,ht=180,ftp=335,wp=20000,cd=0.7185,wb=9.221)
+al = Rider(name='AL',wt=60,ht=170,ftp=300,wp=20000,**bike)
+gm = Rider(name='GM',wt=64,ht=173,ftp=305,wp=20000,**bike)
+ds = Rider(name='DS',wt=81.5,ht=188,ftp=360,wp=20000,**bike)
+ek = Rider(name='EK',wt=76,ht=183,ftp=320,wp=20000,**bike)
+er = Rider(name='ER',wt=81,ht=184,ftp=345,wp=20000,**bike)
+jw = Rider(name='JW',wt=78,ht=185,ftp=311,wp=20000,**bike)
+dk = Rider(name='DK',wt=76,ht=178,ftp=310,wp=20000,**bike)
+ln = Rider(name='LN',wt=77,ht=182,ftp=334,wp=20000,**bike)
+cp = Rider(name='CP',wt=80,ht=183,ftp=300,wp=20000,**bike)
+tr = Rider(name='TR',wt=75,ht=180,ftp=335,wp=20000,**bike)
+sg = Rider(name='SG',wt=68,ht=179,ftp=288,wp=20000,**bike)
+dm1 = Rider(name='DMY',wt=80,ht=182,ftp=290,wp=20000,**bike)
+mb = Rider(name='MB',wt=77.4,ht=180,ftp=310,wp=20000,**bike)
 
+ks = Rider(name='KS',wt=80.1,ht=183,ftp=294,wp=20000,**bike)
+sm = Rider(name='SM',wt=80,ht=187,ftp=281,wp=20000,**bike)
+cm = Rider(name='CM',wt=71,ht=173,ftp=250,wp=20000,**bike)
+pl = Rider(name='PL',wt=73,ht=180,ftp=254,wp=20000,**bike)
+mw = Rider(name='MW',wt=76,ht=180,ftp=275,wp=20000,**bike)
 
+# sb = Rider(name='SB',wt=70.8,ht=175,ftp=269,wp=20000,**bike)
+sb = Rider(name='SB',wt=68.3,ht=175,ftp=294,wp=20000,**bike)
+wy = Rider(name='WA-Y',wt=58,ht=168,ftp=200,wp=20000,**bike)
+dn = Rider(name='DN',wt=69.8,ht=175,ftp=240,wp=20000,**bike)
+cb = Rider(name='CB',wt=78,ht=180,ftp=268,wp=20000,**bike)
+# jm = Rider(name='JM',wt=62,ht=167.6,ftp=245,wp=20000,**bike) #joe monks
+jm = Rider(name='JM',wt=72.8,ht=186,ftp=318,wp=20000,**bike)
 
-# r1 = Rider(name='ER',wt=81,ht=184,ftp=330,wp=20000,cd=0.7185,wb=9.221)
-# r2 = Rider(name='DK',wt=76,ht=178,ftp=322,wp=20000,cd=0.7185,wb=9.221)
-# r3 = Rider(name='SB',wt=67,ht=179,ftp=290,wp=20000,cd=0.7185,wb=9.221)
-# r4 = Rider(name='LN',wt=77,ht=182,ftp=329,wp=20000,cd=0.7185,wb=9.221)
-# r5 = Rider(name='JW',wt=78,ht=185,ftp=311,wp=20000,cd=0.7185,wb=9.221)
-
-# r7 = Rider(name='JM',wt=74,ht=171,ftp=265,wp=20000)
-# r8 = Rider(name='SP',wt=88.9,ht=179,ftp=287,wp=20000)
-# r8 = Rider(name='SB',wt=67,ht=179,ftp=275,wp=20000)
-# r9 = Rider(name='MB',wt=77.4,ht=180,ftp=301,wp=20000)
-
-#step two, define positions in paceline and scaling factor
-pl = Paceline(riders=[r1,r3,r2,r6,r5,r4
-                      ]
-              ,target_speed=47.7
-              ,grade = 0, crr= 0.004)
-
-# pl = Paceline(riders=[r1,r2,r3,r4,r5
-#                       ]
-#               ,target_speed=46.5
-#               ,grade = 0, crr= 0.004)
-
-for r in pl.riders:
-    print(r)
-    print(r.target_power)
-
-#step three, estimate power demands in the paceline
-barray = asymp_func(x=np.arange(0,len(pl.riders),1),x0=0,y0=1.0,x1=4,y1=0.62)
-#print('Draft Fractions by Position:')
-pl.calc_power_demands(base_array=barray)
-
-
-turn_times = [60,45,45,45,30,30]
-# turn_times = [30,30,30,30,30]
-#step four, define turn times in the paceline and estimate duration to create a "workout_dataframe" for each rider
-pl.build_wodf(dur=38*60,
-              turn_times=turn_times
+# riders = [gm,,mb,ln]
+#flat section
+pl_1 = Paceline(riders=[gm,ln,jm,dk,mb]
+              ,target_speed=46.3
+              ,grade = 0.0, crr= 0.004
+              ,distance=12.3*3
+              ,turn_times=[90,45,30,30,30]
               )
+
+
+pls = [pl_1]
+
+
+
+# pls = [pl_1,pl_2,pl_3,pl_4,pl_5,pl_6,pl_7,pl_8
+#        ]
+descs = ['Route'
+         ]
+      
+
+# pls = np.array([pls for x in range(3)]).flatten() #do 2 laps
+# descs = np.array([descs for x in range(3)]).flatten() #do 2 laps
+# pls
+barray = np.array([1,0.85,0.75,0.71,0.65,0.63,0.625,0.62])
+# barray = np.array([1,0.89,0.77,0.69,0.65,0.63,0.62,0.62])
+
+
+total_dur = 0
+for i,pl in enumerate(pls):
+    pl.calc_power_demands(base_array=barray[0:len(pl.riders)])
+    pl.build_wodf()
+    print(descs[i])
+    print(pl.pwr_df)
+    dur = pl.distance/pl.target_speed*60 #hours to minutes)
+    print(dur)
+    total_dur = total_dur+dur 
 
 #step 5, now that each rider has a "workout" based on the power demands from the paceline, calculate wbal over the duration of the event
 fig, ax = plt.subplots()
-for r in pl.riders:
+for r in pl_1.riders:
     r.calc_wbal()
     ax.plot(r.wbal['Time']/60,r.wbal['Wbal'],label=r.name)
 ax.legend()
@@ -228,24 +260,128 @@ ax.set_ylabel('Wbal')
 ax.set_xlabel('Duration (min)')
 ax.set_ylim(0,20000)
 plt.grid()
+#%%
+print('pred Finish Time: '+str(total_dur)+' Minutes')
+avg_speed = np.sum([x.distance for x in pls]) / (total_dur/60) #total distance in km / time in hours
+print('pred Avg. Speed: '+str(avg_speed)+' km/h')
+print('total Distance: '+ str(np.sum([x.distance for x in pls])))
 
-print('Power Demands')
-print(pl.pwr_df)
-#%% printout of pull plan
-pp_df = pl.pwr_df[['rider',0]].rename(columns={0:'Power (w)'})
-pp_df['Turn (s)'] = turn_times
+fig, ax = plt.subplots(figsize=(10,8))
+for r in pl_1.riders:
+    pdf = r.wbal
+    pdf['30sPower'] = pdf['Power'].rolling(window=30).mean()
+    pdf['qcm'] = (pdf['30sPower']**4).cumsum()
+    pdf['NP'] = (pdf['qcm']/pdf['Time'])**0.25
+    pdf['IF'] = pdf['NP']/r.ftp
+    ax.plot(pdf['Time']/60,pdf['NP'],label=r.name)
+ax.legend(loc='lower right')
+ax.set_ylabel('Normalized Power')
+ax.set_xlabel('Duration (min)')
+# ax.set_ylim(0.8,1.2)
+ax.set_ylim(200,400)
+plt.grid() 
+
+
+fig, ax = plt.subplots(figsize=(10,8))
+for r in pl_1.riders:
+    pdf = r.wbal
+    pdf['30sPower'] = pdf['Power'].rolling(window=30).mean()
+    pdf['qcm'] = (pdf['30sPower']**4).cumsum()
+    pdf['NP'] = (pdf['qcm']/pdf['Time'])**0.25
+    pdf['IF'] = pdf['NP']/r.ftp
+    ax.plot(pdf['Time']/60,pdf['IF'],label=r.name)
+ax.legend(loc='lower right')
+ax.set_ylabel('Intensity Factor')
+ax.set_xlabel('Duration (min)')
+ax.set_ylim(0.8,1.2)
+# ax.set_ylim(200,400)
+plt.grid() 
+#%%
+# print('Power Demands')
+# print(pl.pwr_df)
+#printout of pull plan
+pp_df = pl_1.pwr_df[['rider',0]].rename(columns={0:'Power (w)'})
+pp_df['Turn (s)'] = pl_1.turn_times
 pp_df
+#%%
+edf = pd.read_csv(r'C:\Repo\ttt_model\RoadToRuinsStartPier.csv')
+ddf = pd.DataFrame(data={'distance_m':[p.distance for p in pls],'speed_kmh':[p.target_speed for p in pls]})
+ddf['distance_m'] = ddf['distance_m'].cumsum()*1000
+edf = pd.concat([edf,ddf]).sort_values(by='distance_m').reset_index(drop=True).set_index('distance_m')
+edf['altitude_m'] = edf['altitude_m'].interpolate(method='index')
+edf['speed_kmh'] = edf['speed_kmh'].bfill()
+edf = edf.reset_index()
+edf['delta_m'] = (edf['distance_m']-edf['distance_m'].shift(1)).fillna(0)
+edf['Time_min'] = (edf['delta_m']/1000) / (edf['speed_kmh'])*60
+edf['Time_min'] = edf['Time_min'].cumsum()
+edf
+
+#%%
+from bokeh.plotting import figure, save
+from bokeh.layouts import column
+from bokeh.models import ColumnDataSource, Range1d, DataRange1d, LinearAxis, CustomJSTickFormatter
+
+
+p = figure(height=500,width=2000)
+p.xaxis[0].axis_label = 'Time (min)'
+p.yaxis[0].axis_label = "W'bal"
+
+
+src_dict = {}
+for r in pl_1.riders:
+    df = r.wbal.copy()
+    df['Time'] = df['Time']/60
+    src_dict[r.name] = ColumnDataSource(df)
+
+elev_src = ColumnDataSource(edf)
+
+p.extra_y_ranges['elev'] = DataRange1d()
+elev_rend = p.varea(x='Time_min',y2='altitude_m',y1=edf['altitude_m'].min(),fill_alpha=0.5,source=elev_src,y_range_name='elev',fill_color='grey')
+p.extra_y_ranges['elev'].renderers=[elev_rend]
+ax2 = LinearAxis(y_range_name="elev", axis_label="Elevation (m)")
+p.add_layout(ax2, 'right')
+
+dax = LinearAxis(axis_label='Distance (km)')
+p.add_layout(dax,'below')
+
+ct = CustomJSTickFormatter(args=dict(src=elev_src)
+                           ,code='''
+
+                           for (var i = 0; i < src.data['Time_min'].length; i++){
+                                   
+                                   if (src.data['Time_min'][i]>=tick){
+                                           break
+                                           }
+                                   }
+                           var d2 = src.data['distance_m'][i]/1000
+                           var d1 = src.data['distance_m'][i-1]/1000
+                           var t2 = src.data['Time_min'][i]
+                           var t1 = src.data['Time_min'][i-1]
+                           var m = (d2-d1)/(t2-t1)
+                           var b = d1 - m*t1
+                           
+                           return Math.round(b+m*tick,0)
+                           ''')
+dax.formatter = ct           
+
+from bokeh.palettes import Category20
+for i,k in enumerate(src_dict.keys()):
+    pl = Category20[len(src_dict.keys())]
+    r = p.line(x='Time',y='Wbal',line_width=3,line_color=pl[i],source=src_dict[k],legend_label=k)
+
+save(p,r'C:\Repo\ttt_model\Viewer.html')
+
+
+
+
+
+
+
 
 # (pp_df['Turn (s)'].sum()+45+30+30)*47000/60/60
 
  #%%           
-pl = Paceline(riders=[r2,r3,r6,r5,r4,r1
-                      ]
-              ,target_speed=33
-              ,grade = 3.1, crr= 0.004)
-for r in pl.riders:
-    print(r)
-    print(r.target_power)
+
 
     
     
